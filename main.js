0dd0699f6b7d9d0dd0e02aa02e4d39c0bf0349e9
Sanjay Salve <sanjay.SHVinfo@gmail.com>
2023-11-24 17:55:54 +0530

    main

--- main.js
+++ main.js
@@ -8,6 +8,13 @@ $(document).ready(function () {
   });
   
 
+  // sticky nav
+
+// window.addEventListener("scroll", function () {
+//   let nav = document.querySelector(".navigate_bar");
+//   nav.classList.toggle("sticky", window.scrollY > 50);
+// });
+
 
   $('.brand_slider').owlCarousel({
     loop:true,
